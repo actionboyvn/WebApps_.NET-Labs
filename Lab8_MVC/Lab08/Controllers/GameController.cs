23645af3ecaafea070e5f225957a89aeb00195b1
Van Hien Le <95376510+actionboyvn@users.noreply.github.com>
2023-01-30 21:59:03 +0100

    Add files via upload

---
+++ Lab8_MVC/Lab08/Controllers/GameController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab08.Controllers
+{
+    public class GameController : Controller
+    {
+        static readonly Random random = new Random();
+        static int n = 0;
+        static int randValue = 0;
+        static int tries = 0;        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Set(int newRange)
+        {
+            n = newRange;
+            ViewBag.n = n;
+            return View("Game");
+        }
+        public IActionResult Draw()
+        {
+            ViewBag.n = n;
+            randValue = random.Next(0, n);
+            tries = 0;
+            if (n == 0)
+                ViewBag.sms = "You must set the range first!";
+            else
+                ViewBag.sms = "A new random number has been drawn. You can start the game!";
+            ViewBag.color = "blue";
+            return View("Game");
+        }
+        public IActionResult Guess(int x)
+        {
+            ViewBag.n = n;
+            tries++;
+            if (x == randValue)
+            {
+                ViewBag.sms = String.Format("You have guessed it right after {0} tries", tries);
+                ViewBag.color = "blue";
+            }
+            if (x < randValue)
+            {
+                ViewBag.sms = String.Format("Try it again. You guessed too small! You have guessed {0} times.", tries);
+                ViewBag.color = "green";
+            }
+            if (x > randValue)
+            {
+                ViewBag.sms = String.Format("Try it again. You guessed too big! You have guessed {0} times.", tries);
+                ViewBag.color = "red";
+            }
+            return View("Game");
+        }
+    }
+}
---
+++ Lab8_MVC/Lab08/Controllers/ToolController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab08.Controllers
+{
+    public class ToolController : Controller
+    {        
+        public IActionResult Solve(double a, double b, double c)
+        {            
+            ViewBag.color = "cyan";
+            ViewBag.res = "";
+            double delta = b * b - 4 * a * c;
+            if (a == 0 && b == 0)
+            {
+                if (c == 0)
+                    ViewBag.res = "Identity.";
+                else
+                {
+                    ViewBag.res = "No solution.";
+                    ViewBag.color = "red";
+                }
+                return View();
+            }
+            if (a == 0)
+            {
+                ViewBag.res = String.Format("Sol = {0:0.00}", -c / b);
+                ViewBag.color = "pink";
+                return View();
+            }
+            if (delta == 0)
+            {
+                ViewBag.res = String.Format("Sol1 = Sol2 = {0:0.00}", -b / 2 / a);
+                ViewBag.color = "blue";                
+            }
+            if (delta > 0)
+            {
+                ViewBag.res = String.Format("Sol1 = {0:0.00}, Sol2 = {1:0.00}", 
+                    (-b - Math.Sqrt(delta)) / 2 / a, (-b + Math.Sqrt(delta)) / 2 / a);                
+                ViewBag.color = "green";                
+            }
+            if (delta < 0)
+            {
+                ViewBag.res = "No solution.";
+                ViewBag.color = "red";
+            }
+            return View();
+        }
+    }
+}

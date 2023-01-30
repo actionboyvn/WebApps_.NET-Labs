d5ed8d01efa98d0cc5fbf2f1db6a8644de581f63
Van Hien Le <95376510+actionboyvn@users.noreply.github.com>
2023-01-30 22:04:51 +0100

    Add files via upload

---
+++ Lab11_Cookies/Lab102/Controllers/CategoriesController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab102.Data;
+using Lab102.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Lab102.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly Context _context;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+
+        public CategoriesController(Context context, IHostingEnvironment hostingEnvironment)
+        {
+            _context = context;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Categories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(category);          
+            var context = _context.Articles
+                .Include(a => a.Category)
+                .Where(a => a.CategoryId == id);
+            var articles = context.ToList();
+            for (int i = 0; i < articles.Count; i++)
+            {
+                var article = articles[i];              
+                if (article.imagePath is not null)
+                {
+                    string uploadFolder = Path.Combine(_hostingEnvironment.WebRootPath, "upload");
+                    string filePath = Path.Combine(uploadFolder, article.imagePath);
+                    if (System.IO.File.Exists(filePath))
+                        System.IO.File.Delete(filePath);
+                }
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Lab11_Cookies/Lab102/Controllers/ShopController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab102.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Lab102.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly Context _context;
+        public ShopController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }       
+        
+        [HttpPost]
+        public async Task<IActionResult> Details(int? Id)
+        {
+            if (Id is null)
+                return RedirectToAction(nameof(Index));
+            var context = _context.Articles
+                .Include(a => a.Category)
+                .Where(a => a.CategoryId == Id);           
+            ViewData["Category"] = "Category: " + _context.Categories.First(c => c.Id == Id).Name;          
+            return View(await context.ToListAsync());
+        }
+        public IActionResult Cart()
+        {
+            Dictionary<int, int> quantities = getQuantities();
+            ViewBag.nums = quantities;
+            ViewBag.totalCost = calcTotalCost();
+            var context = _context.Articles.
+                Include(a => a.Category).
+                Where(a => quantities.Keys.Contains(a.Id));             
+            return View(context);
+        }
+
+        public Dictionary<int, int> getQuantities()
+        {
+            string? temp;
+            Request.Cookies.TryGetValue("quantities", out temp);
+            if (temp is not null)
+            {
+                return JsonConvert.DeserializeObject<Dictionary<int, int>>(Request.Cookies["quantities"]);
+            }
+            else
+                return new Dictionary<int, int>();
+        }
+        public float calcTotalCost()
+        {            
+            Dictionary<int, int> quantities = getQuantities();
+            var articles = _context.Articles.ToList();            
+            float totalCost = 0;                                  
+            for (int i = 0; i < articles.Count(); i++)
+            {
+                if (quantities.ContainsKey(articles[i].Id))
+                    totalCost += quantities[articles[i].Id] * articles[i].Price;
+            }
+            return totalCost;
+        }
+        public void saveQuantities(Dictionary<int, int> quantities)
+        {
+            string temp = JsonConvert.SerializeObject(quantities);
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddDays(7);          
+            Response.Cookies.Append("quantities", temp, options);
+        }
+
+        public IActionResult AddToCart(int id)
+        {
+            Dictionary<int, int> quantities = getQuantities();
+            if (quantities.ContainsKey(id))
+                quantities[id]++;
+            else
+                quantities.Add(id, 1);
+            saveQuantities(quantities);            
+            return RedirectToAction(nameof(Cart));
+        }
+   
+        public IActionResult ReduceFromCart(int id)
+        {
+            Dictionary<int, int> quantities = getQuantities();
+            if (quantities.ContainsKey(id))
+            {
+                quantities[id]--;
+                if (quantities[id] == 0)
+                    quantities.Remove(id);
+            }
+            saveQuantities(quantities);
+            return RedirectToAction(nameof(Cart));
+        }
+    }
+}
